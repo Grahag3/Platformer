09e5d08d797c7ca86f9b3ad5103e6022bb3a7e9b
grahag3 <grahag3@rpi.edu>
2019-04-19 14:23:14 -0400

    Began work on stage building and dropper obstacles

---
+++ Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jump_force;
+    public float speed;
+
+    public bool jumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            transform.position += Vector3.right * speed;
+        }
+
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            transform.position += -Vector3.right * speed;
+        }
+    }
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (jumping == false)
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                if (Input.GetKeyDown(KeyCode.Space) == true)
+                {
+                    GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jump_force), ForceMode2D.Impulse);
+
+                    jumping = true;
+
+                }
+            }
+        }
+        
+    }
+}
---
+++ Platformer/Assets/Scripts/PressurePad.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePad : MonoBehaviour
+{
+    public GameObject spike1;
+    public GameObject spike2;
+    public GameObject spike3;
+    public GameObject spike4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+       
+
+      
+        {
+            StartCoroutine(break_blocker());
+           
+             
+            
+
+            
+          
+        }
+    }
+
+    IEnumerator break_blocker()
+    {
+        GameObject[] blockers = GameObject.FindGameObjectsWithTag("Blocker");
+
+        foreach (var item in blockers) { 
+        yield return new WaitForSeconds(.5f);
+        Destroy(item);
+        }
+    }
+}
